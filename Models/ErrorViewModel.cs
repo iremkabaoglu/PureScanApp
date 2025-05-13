35d5ac05f4a6e5e08b690cc77e45aaebc5d18a58
İrem Kabaoğlu <iremkabaoglu1@gmail.com>
2025-05-13 11:38:46 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PureScanApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
