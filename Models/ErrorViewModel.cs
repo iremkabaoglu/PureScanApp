9a16dfe7bf1241f161c555369e6fabe0ebb7fbf1
İrem Kabaoğlu <iremkabaoglu1@gmail.com>
2025-05-23 18:05:48 +0300

    Önemsiz dosyalar dışlandı, model entegrasyonu tamamlandı

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PureScanApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
